ac1feab43a832305e25f253b55dd491ba89c8e52
Paul LaRue <whiteout.paul@gmail.com>
2019-09-15 12:00:23 -0400

    fov started
    added some fov variables and new colors

--- venv/engine.py
+++ venv/engine.py
@@ -8,7 +8,7 @@ from map_objects.game_map import GameMap
 
 def main():
 
-    # Game and map constants
+        # Game and map constants
     screen_width = 80
     screen_height = 50
 
@@ -19,9 +19,16 @@ def main():
     room_min_size = 6
     max_rooms = 30
 
+        # FoV Variables
+    fov_algoritm = 0
+    fov_light_walls = True
+    fov_radius = 10
+
     colors = {
         'dark_wall': tcod.Color(0,0,100),
-        'dark_ground': tcod.Color(50,50,150)
+        'dark_ground': tcod.Color(50,50,150),
+        'light_wall': tcod.Color(130, 110, 50),
+        'light_ground': tcod.Color(200,180,50)
     }
 
 
@@ -37,6 +44,8 @@ def main():
 
     game_map = GameMap(map_width, map_height)
     game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
+
+    fov_recompute = True
 
     key = tcod.Key()
     mouse = tcod.Mouse()
