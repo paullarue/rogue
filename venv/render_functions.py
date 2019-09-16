f23c27150a9c98f9c531652e255ed170df7d06d5
Paul LaRue <whiteout.paul@gmail.com>
2019-09-16 17:38:10 -0400

    fov exploration complete

--- venv/render_functions.py
+++ venv/render_functions.py
@@ -13,15 +13,15 @@ def render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, sc
                         tcod.console_set_char_background(con, x, y, colors.get('light_wall'), tcod.BKGND_SET)
                     else:
                         tcod.console_set_char_background(con, x, y, colors.get('light_ground'), tcod.BKGND_SET)
-
-                else:
+                    game_map.tiles[x][y].explored = True
+                elif game_map.tiles[x][y].explored:
                     if wall:
                         tcod.console_set_char_background(con, x, y, colors.get('dark_wall'), tcod.BKGND_SET)
                     else:
                         tcod.console_set_char_background(con, x, y, colors.get('dark_ground'), tcod.BKGND_SET)
 
     for entity in entities:
-        draw_entity(con, entity)
+        draw_entity(con, entity, fov_map)
 
     tcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
 
@@ -29,9 +29,10 @@ def clear_all(con, entities):
     for entity in entities:
         clear_entity(con, entity)
 
-def draw_entity(con, entity):
-    tcod.console_set_default_foreground(con, entity.color)
-    tcod.console_put_char(con, entity.x, entity.y, entity.char, tcod.BKGND_NONE)
+def draw_entity(con, entity, fov_map):
+    if tcod.map_is_in_fov(fov_map, entity.x, entity.y):
+        tcod.console_set_default_foreground(con, entity.color)
+        tcod.console_put_char(con, entity.x, entity.y, entity.char, tcod.BKGND_NONE)
 
 def clear_entity(con, entity):
     tcod.console_put_char(con, entity.x, entity.y, ' ', tcod.BKGND_NONE)
