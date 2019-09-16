25ded90fa8dafee3c176d46e57055abfb148f3a3
Paul LaRue <laruep@rpi.edu>
2019-09-15 19:38:12 -0700

    i fucked up

--- venv/map_objects/rectangle.py
+++ venv/map_objects/rectangle.py
@@ -5,3 +5,10 @@ class Rect:
         self.y1 = y
         self.x2 = x + w
         self.y2 = y + h
+
+    def center(self):
+        center_x = int((self.x1 + self.x2) / 2)
+        center_y = int((self.y1 + self.y2) / 2)
+        return (center_x , center_y)
+
+    def intersect(self, other):
